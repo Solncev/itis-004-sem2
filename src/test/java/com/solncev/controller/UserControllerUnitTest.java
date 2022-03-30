98e87656f3c1145df37a603b8dfdac576e74af26
maratsolncev <maratsolncev@piano.io>
2022-03-30 09:33:43 +0300

    spring unit and integration tests, mockito, jacoco

---
+++ src/test/java/com/solncev/controller/UserControllerUnitTest.java
@@ -0,0 +1,53 @@
+package com.solncev.controller;
+
+import com.solncev.dto.UserDto;
+import com.solncev.model.User;
+import com.solncev.repository.UserRepository;
+import com.solncev.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Before
+    public void init() {
+        User user  = new User();
+        user.setName("testName");
+        given(userService.getAll()).willReturn(Arrays.asList(UserDto.fromModel(user)));
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+        mockMvc.perform(get("/user")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name").value("testName"));
+    }
+}

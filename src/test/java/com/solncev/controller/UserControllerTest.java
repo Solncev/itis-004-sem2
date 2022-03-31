eda8478458f34fcdd47e94588573ebc1eea83c39
maratsolncev <maratsolncev@piano.io>
2022-03-31 15:22:32 +0300

    test for post method

--- src/test/java/com/solncev/controller/UserControllerTest.java
+++ src/test/java/com/solncev/controller/UserControllerTest.java
@@ -1,21 +1,33 @@
 package com.solncev.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.solncev.Application;
+import com.solncev.dto.CreateUserDto;
+import com.solncev.dto.UserDto;
 import com.solncev.model.User;
 import com.solncev.repository.UserRepository;
+import com.solncev.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -30,6 +42,11 @@ public class UserControllerTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @MockBean
+    private UserService userService;
+
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Before
     public void init() {
@@ -58,5 +75,22 @@ public class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("sign_up"));
+    }
+
+    @Test
+    public void testRegistration() throws Exception {
+        CreateUserDto createUserDto = new CreateUserDto();
+        createUserDto.setName("Boris");
+        createUserDto.setPassword("testTEST");
+        createUserDto.setEmail("boris@mail.ru");
+        given(userService.signUp(any(CreateUserDto.class), anyString())).willReturn(new UserDto(1, "Boris", "boris@mail.ru"));
+        mockMvc.perform(post("/sign_up")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .flashAttr("user", createUserDto)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("sign_up_success"));
     }
 }
